2fa312b4c702aa82641e0db73bb148ab2bed0b53
Nucleus <nucleus-bot@uipath.com>
2026-03-12 17:06:52 +0530

    chore: Update bootstrap script with latest commands

--- .bootstrap.js
+++ .bootstrap.js
@@ -77,7 +77,9 @@ function updateWranglerJsonc() {
 }
 
 function runSetupCommands() {
-    const commands = [];
+    const commands = [
+    "bun add @tanstack/react-table@^8.20.0"
+];
     
     if (commands.length === 0) {
         console.log('⊘ No setup commands to run');
